98c22c96ef61c654831745cbabc17280f2a1566d
Hashim271 <hashim46@live.ca>
2025-04-03 19:45:30 -0400

    Added Keyboard Event

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
     hoverBtn.addEventListener('mouseleave', () => {
         mouseMessage.textContent = ('');
     });
+
+
+    // Keyboard Events
+    const keyInput = document.getElementById('keyInput');
+    const keyDisplay = document.getElementById('keyDisplay');
+
+    keyInput.addEventListener('keyup', (x) => {
+        keyDisplay.textContent = x.key;
+    });
 });
